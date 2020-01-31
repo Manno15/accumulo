a705444a46427ffc83b40088e984e5975b7f7fcf
Jeffrey Manno <jeffreymanno@gmail.com>
2020-01-31 10:17:54 -0500

    more revert

--- server/tserver/src/main/java/org/apache/accumulo/tserver/TabletServer.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/TabletServer.java
@@ -2536,10 +2536,7 @@ public class TabletServer extends AbstractServer {
             resourceManager.createTabletResourceManager(extent, getTableConfiguration(extent));
         TabletData data = new TabletData(extent, fs, tabletMetadata);
         tablet = new Tablet(TabletServer.this, extent, trm, data);
-        if(data.getLastLocation().toString().equals(getClientAddressString())) {
-          log.info("EQUAL TO client address");
-          tablet.updateLastLocation(System.currentTimeMillis());
-        }
+
         // If a minor compaction starts after a tablet opens, this indicates a log recovery
         // occurred. This recovered data must be minor compacted.
         // There are three reasons to wait for this minor compaction to finish before placing the
@@ -2559,7 +2556,10 @@ public class TabletServer extends AbstractServer {
         }
 
         Assignment assignment = new Assignment(extent, getTabletSession());
-
+        if(!data.getLastLocation().toString().equals(getClientAddressString())) {
+          log.info("EQUAL TO client address");
+          tablet.updateLastLocation(System.currentTimeMillis());
+        }
 
 
         TabletStateStore.setLocation(getContext(), assignment);
